a31ff800d78d8a433942ad13e15c46f9bb27db16
Artem <>
2024-08-12 10:14:45 +0700

    + fix + version bump

--- mad_koin_compose/src/main/java/ru/wearemad/mad_koin_compose/scopes/DefaultOpenedScopesHolder.kt
+++ mad_koin_compose/src/main/java/ru/wearemad/mad_koin_compose/scopes/DefaultOpenedScopesHolder.kt
@@ -9,21 +9,26 @@ class DefaultOpenedScopesHolder : OpenedScopesHolder {
         private const val KEY_OPENED_SCOPES = "ru.wearemad.mad_koin_compose.scopes.OpenedScopesHolder"
     }
 
+    @get:Synchronized
     private val openedScopesSet: ScreenScopes = mutableSetOf()
 
+    @get:Synchronized
     override val openedScopes: Set<String>
         get() = openedScopesSet.toSet()
 
+    @Synchronized
     override fun removeScreenScope(
         screenId: String
     ) {
         openedScopesSet.remove(screenId)
     }
 
+    @Synchronized
     override fun addScreenScope(screenId: String) {
         openedScopesSet.add(screenId)
     }
 
+    @Synchronized
     override fun saveState(): Bundle = Bundle().apply {
         putStringArray(
             KEY_OPENED_SCOPES,
@@ -31,6 +36,7 @@ class DefaultOpenedScopesHolder : OpenedScopesHolder {
         )
     }
 
+    @Synchronized
     override fun restoreState(inState: Bundle) {
         val scopes = inState.getStringArray(KEY_OPENED_SCOPES) ?: return
         openedScopesSet.addAll(
@@ -38,6 +44,7 @@ class DefaultOpenedScopesHolder : OpenedScopesHolder {
         )
     }
 
+    @Synchronized
     override fun clearAll() {
         openedScopesSet.clear()
     }
